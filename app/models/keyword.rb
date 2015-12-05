2318b6df04a9ce45cce8c8359a5a3ccdfd74c563
MohamedYakout <mohamed.yakout91@gmail.com>
2015-12-06 01:43:27 +0200

    Add parsing the google result page & create urls for keyword

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -11,7 +11,11 @@ class Keyword < ActiveRecord::Base
 		spreadsheet = open_spreadsheet(file)
 		header = spreadsheet.row(1)
 		(1..spreadsheet.last_row).each do |i|
-			puts spreadsheet.row(i)
+			row = spreadsheet.row(i)
+			row.each do |word|
+				keyword = Keyword.find_or_create_by(word: word)
+				keyword.delay.search_on_google
+			end
 		end
 	end
 
@@ -20,25 +24,6 @@ class Keyword < ActiveRecord::Base
 		when ".csv" then Roo::CSV.new(file.path, csv_options: {encoding: "iso-8859-1:utf-8"})
 		else raise "Unknown file type: #{file.original_filename}"
 		end
-	end
-
-	def self.search_google(words)
-		words.gsub! ' ', '+'
-		url = URI.parse("https://www.google.com.eg/?gfe_rd=cr&gws_rd=cr#q=#{words}")
-		req = Net::HTTP::Get.new(url.to_s)
-		binding.pry
-		res = Net::HTTP.start(url.host, url.port) {|http|
-		  http.request(req)
-		}
-		# puts res.body
-		res
-	end
-
-	def self.https_google_search
-		http = HTTPClient.new
-		binding.pry
-		response = http.get_content("https://www.google.com/")
-		response
 	end
 
 	def search_on_google
@@ -53,8 +38,39 @@ class Keyword < ActiveRecord::Base
 		request = Net::HTTP::Get.new( uri.path+ '?' + request.body ) 
 
 		response = http.request(request)
+		binding.pry
 		self.page = response.read_body
+		self.page.force_encoding("ISO-8859-1").encode("UTF-8")
 		self.save!
+		self.parsing_html
+	end
+
+	def parsing_html
+		html_doc = Nokogiri::HTML(self.page)
+		top_ads_nodes = html_doc.css("#center_col li.ads-ad h3 a")
+		right_ads_nodes = html_doc.css("#rhs_block li.ads-ad h3 a")
+		non_ads_nodes = html_doc.css("#ires li h3 a")
+
+		top_ads_nodes.each do |node|
+			link = node.attributes["href"].value
+			page_url = link.match(/http.*/)[0]
+			puts page_url
+			self.ad_urls.create(link: page_url, position: "top", is_ad: true)
+		end
+
+		right_ads_nodes.each do |node|
+			link = node.attributes["href"].value
+			page_url = link.match(/http.*/)[0]
+			puts page_url
+			self.ad_urls.create(link: page_url, position: "right", is_ad: true)
+		end
+
+		non_ads_nodes.each do |node|
+			link = node.attributes["href"].value
+			page_url = link.match(/http.*/)[0]
+			puts page_url
+			self.ad_urls.create(link: page_url, position: "center", is_ad: false)
+		end
 	end
 
 	# Keyword.search_google("ruby rails")
